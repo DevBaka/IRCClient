f271f618ca46a3d57d0b4d51fa012dbf853750f5
Unknown <devbaka@gmx.net>
2018-01-31 12:22:50 +0100

    gui actionlistender

--- src/irc/ircFunctions.java
+++ src/irc/ircFunctions.java
@@ -3,18 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package irc;
 
 /**
  *
  * @author devbaka
  */
-
-
-public class IRCClient {
-    public static void main(String[] args)
-    {
-        //mainWindow mainWindow = new mainWindow();
-        gui.mainWindow mainWindow = new gui.mainWindow();
-    }
+public class ircFunctions {
+    
     
 }
